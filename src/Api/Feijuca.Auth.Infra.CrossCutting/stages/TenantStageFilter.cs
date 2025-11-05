c333f33e41f5255388155e7905e08a60e0eecd8a
Guilherme Giannini <guilherme.giannini@smartinnovate.com.br>
2025-11-05 16:13:02 -0300

    movendo classe

---
+++ src/Api/Feijuca.Auth.Infra.CrossCutting/stages/TenantStageFilter.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text.RegularExpressions;
+
+namespace Feijuca.Auth.Infra.CrossCutting.stages
+{
+    public static class TenantStageFilter
+    {
+        public static FilterDefinition<BsonDocument> MatchByTenants(IEnumerable<string> tenantNames)
+        {
+            var names = tenantNames.Select(x => new BsonRegularExpression(new Regex(x, RegexOptions.IgnoreCase)));
+
+            var filter = new BsonDocument(
+                "Tenants",
+                new BsonDocument("$in", BsonArray.Create(names)));
+
+            return new BsonDocumentFilterDefinition<BsonDocument>(filter);
+        }
+
+        public static FilterDefinition<BsonDocument> MatchByTenant(string tenant)
+        {
+            var filter = new BsonDocument(
+                "Tenant",
+                new BsonDocument("$eq", tenant));
+
+            return new BsonDocumentFilterDefinition<BsonDocument>(filter);
+        }
+    }
+}
